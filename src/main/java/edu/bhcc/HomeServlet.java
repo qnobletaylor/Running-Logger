ab24f99f8cc8c008fc6ac9e145f7442b1792e3fa
Quinlin Taylor <102311011+qnobletaylor@users.noreply.github.com.>
2024-11-16 10:14:20 -0500

    Added comments to all neccasary files

--- src/main/java/edu/bhcc/HomeServlet.java
+++ src/main/java/edu/bhcc/HomeServlet.java
@@ -9,10 +9,19 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-
+/**
+ * Servlet for displaying data at the home address of localhost:8080. Handles both get and post requests. In both
+ * cases the same html will be sent so there is a file located at "src/main/webapp/WEB-INF/index.html" which contains
+ * the page source. I felt like writing a file and then reading the file with a Scanner was a much cleaner way to
+ * send the response. I probably would have done a html source file for the RunServlet as well but this was before
+ * our class on 11/15 learning about java templating, so I decided to stick with what we'd learned previously.
+ */
 public class HomeServlet extends HttpServlet {
 
 
+    /**
+     * Replies to the client with html on get request.
+     */
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
 
@@ -28,6 +37,10 @@ public class HomeServlet extends HttpServlet {
         }
     }
 
+    /**
+     * Replies to client with html on post request. I realize this could have been not included, but I made the button
+     * on localhost:8080/Run have a post method.
+     */
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
         throws IOException {
 
--- src/main/java/edu/bhcc/RunServlet.java
+++ src/main/java/edu/bhcc/RunServlet.java
@@ -3,45 +3,65 @@ package edu.bhcc;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.*;
 import java.util.ArrayList;
 
+/**
+ * This class handles both post and get requests for localhost:8080/Run. On post, it receives data from an html
+ * form to input a Run record into the sqlite database. For both requests it will also read all data from the
+ * database and send it to the browser to be displayed.
+ */
+public class RunServlet extends HttpServlet {
 
-public class NewRunServlet extends HttpServlet {
-
-
+  /**
+   * record class datatype to hold records from the sqlite database when being read from.
+   * @param date of an activity
+   * @param distance traveled during the activity
+   * @param time spent during activity
+   * @param speed at which the activity was performed
+   * @param stravaID activity id for strava
+   */
   private record Run(String date, double distance, double time, double speed, String stravaID){}
-  private ArrayList<Run> runs;
-  private Connection database;
+  private ArrayList<Run> runs; // holds the runs from database before being sent through the servlet response
+  private Connection database; // connection to sqlite database
 
 
-
+  /**
+   * Reply to a post request, establishes connection to the database and inserts a new record from the html form sent
+   * with the request. Next will respond by sending the updated list of runs to be viewed in the browser.
+   */
   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
 
     try {
       database = DriverManager.getConnection("jdbc:sqlite:src/main/webapp/WEB-INF/running.db");
-      insertRun(database, request);
-      runs = parseRuns(database);
-      sendHtml(response, runs);
-      database.close();
+
+      insertRun(database, request); // create new database record
+      runs = parseRuns(database); // initialize arrayList with all database records
+      sendHtml(response, runs); // respond to client
+
+      database.close(); // close sqlite connection
     } catch (SQLException e) {
       throw new RuntimeException(e);
     }
 
   }
 
+  /**
+   * Handles a get request. So that if you enter localhost:8080/Run you'll be greeted with every activity that has
+   * already by submitted into the database.
+   */
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
 
     try {
       database = DriverManager.getConnection("jdbc:sqlite:src/main/webapp/WEB-INF/running.db");
-      runs = parseRuns(database);
-      sendHtml(response, runs);
-      database.close();
+
+      runs = parseRuns(database); // initialize arrayList with all database records
+      sendHtml(response, runs); //  respond to client
+
+      database.close(); // close sqlite connection
     } catch (SQLException e) {
       throw new RuntimeException(e);
     }
@@ -49,28 +69,35 @@ public class NewRunServlet extends HttpServlet {
 
   }
 
-
+  /**
+   * Takes a servlet response as well as the runs ArrayList and sends the html line by line back to the client. Will
+   * loop through the ArrayList so that all runs are added to the page to be displayed.
+   */
   private void sendHtml(HttpServletResponse response, ArrayList<Run> runs) throws IOException {
     response.setContentType("text/html");
     response.setStatus(HttpServletResponse.SC_OK);
 
     PrintWriter out = response.getWriter();
+
     out.println("<!DOCTYPE html>");
     out.println("<html lang='en'>");
     out.println("<body>");
     out.println("<div style=height:500px;width:450px;overflow:auto>");
-    for (Run run : runs) {
+
+    for (Run run : runs) { // insert each run into the page
       out.println("<h3>" + run.date + "</h3>");
       out.println("<p>Distance : " + String.format("%.2f", run.distance) + " miles</p>");
       out.println("<p>Duration : " + String.format("%d : %d : %d", ((int) run.time / 3600),
           ((int) run.time % 3600) / 60, ((int) run.time % 60)) + "</p>");
       out.println("<p>Speed    : " + String.format("%.2f", run.speed) + " min/mile</p>");
-      if (run.stravaID != null) {
+
+      if (run.stravaID != null) { // Checks if there is a strava id present
         out.println("<div class='strava-embed-placeholder' data-embed-type='activity' data-embed-id='"+run.stravaID+"' " +
             "data-style='standard'></div><script src='https://strava-embeds.com/embed.js'></script>\n");
       }
       out.println("<h2>--------------------------------------------------------</h2>");
     }
+
     out.println("</div>");
     out.println("<form action='/' method='post'>");
     out.println("<input type='submit' value='New Run'>");
@@ -79,6 +106,11 @@ public class NewRunServlet extends HttpServlet {
   }
 
 
+  /**
+   * Inserts a record into the sqlite database, takes care of parsing the html form data so that it will fit
+   * within the database. The only variable which is checked for whether it's empty or not is stravaID, all others are
+   * left unchecked because the html input itself already implements checking for valid data.
+   */
   private void insertRun(Connection connection, HttpServletRequest request) throws SQLException{
     String date = request.getParameter("date");
     double distance = Double.parseDouble(request.getParameter("distance"));
@@ -88,7 +120,7 @@ public class NewRunServlet extends HttpServlet {
     double time = timeHr * 3600 + timeMin * 60 + timeSec; // time stored in seconds
     String stravaID = request.getParameter("stravaID").isEmpty() ? null : request.getParameter("stravaID");
 
-
+    // prepared statement for record insertion
     PreparedStatement insert = connection.prepareStatement("INSERT INTO RUNS(Date, Distance, Time, stravaID) VALUES" +
         "(?, ?, ?, ?)");
     insert.setString(1, date);
@@ -99,6 +131,12 @@ public class NewRunServlet extends HttpServlet {
     insert.executeUpdate();
   }
 
+  /**
+   * Queries the database for all records in the Runs column and inserts it into the ArrayList of Run. The speed
+   * attribute is derived by the database itself when a record is inserted.
+   *
+   * @return the ArrayList containing all records from database
+   */
   private ArrayList<Run> parseRuns(Connection connection) throws SQLException {
     ResultSet query = connection.createStatement().executeQuery("SELECT * FROM RUNS");
     ArrayList<Run> runs = new ArrayList<>();
@@ -117,6 +155,12 @@ public class NewRunServlet extends HttpServlet {
     return runs;
   }
 
+  /**
+   * Somewhat of a failsafe to ensure that form data for time is actually an int (if the form was left blank it would
+   * throw NumberFormatException, in which case we want to set that field to 0
+   *
+   * @return either the parsed int from string or zero if no int can be parsed
+   */
   private int validateInt(String input) {
     try {
       return Integer.parseInt(input);
